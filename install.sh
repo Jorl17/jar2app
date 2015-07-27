c5deb1876f206bb77b91ff8bde5591f53c0574ca
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-27 16:06:16 +0100

    Avoid recreating directories when installing

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ if [ -z "$1" ]
     PREFIX=$1
 fi
 
-mkdir $PREFIX/jar2app_basefiles
+if [ ! -e  $PREFIX/jar2app_basefiles ]
+then
+  mkdir $PREFIX/jar2app_basefiles
+fi
 cp -r jar2app_basefiles/* $PREFIX/jar2app_basefiles
 cp jar2app $PREFIX
 chmod +x $PREFIX/jar2app
