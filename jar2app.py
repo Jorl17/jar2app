5a29d64bc24d514cc406cd2fb71a72dee04880a0
João Ricardo Lourenço <jorl17.8@gmail.com>
2015-07-27 15:59:49 +0100

    Copyright notice

--- jar2app.py
+++ jar2app.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+##
+## Copyright (C) 2015 João Ricardo Lourenço <jorl17.8@gmail.com>
+##
+## Github: https://github.com/Jorl17
+##
+## Project main repository: https://github.com/Jorl17/jar2app
+##
+## This file is part of jar2app.
+##
+## jar2app is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## jar2app is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with jar2app.  If not, see <http://www.gnu.org/licenses/>.
+##
 from optparse import OptionParser
 import os.path
 import shutil
